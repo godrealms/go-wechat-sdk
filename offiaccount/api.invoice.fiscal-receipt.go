359f1d9d5202a85efd9a8194bbd90f4636c5affa
wujie <wujie812@qq.com>
2026-04-14 11:08:18 +0800

    fix(offiaccount): wire AccessTokenE in fiscal receipt invoice APIs

--- offiaccount/api.invoice.fiscal-receipt.go
+++ offiaccount/api.invoice.fiscal-receipt.go
@@ -1,15 +1,21 @@
 package offiaccount
 
 import (
+	"context"
+	"fmt"
 	"net/url"
 )
 
 // GetFiscalAuthData 查询财政电子票据授权信息
 // https://developers.weixin.qq.com/doc/service/api/invoice/FiscalReceipt/api_invoicebizgetauthdata.html
-func (c *Client) GetFiscalAuthData(request *GetFiscalAuthDataRequest) (*GetFiscalAuthDataResult, error) {
-	result := &GetFiscalAuthDataResult{}
-	err := c.Https.Post(c.ctx, "/card/invoice/getauthdata", request, result)
+func (c *Client) GetFiscalAuthData(ctx context.Context, request *GetFiscalAuthDataRequest) (*GetFiscalAuthDataResult, error) {
+	token, err := c.AccessTokenE(ctx)
 	if err != nil {
+		return nil, err
+	}
+	result := &GetFiscalAuthDataResult{}
+	path := fmt.Sprintf("/card/invoice/getauthdata?access_token=%s", token)
+	if err := c.Https.Post(ctx, path, request, result); err != nil {
 		return nil, err
 	}
 	return result, nil
@@ -17,11 +23,17 @@ func (c *Client) GetFiscalAuthData(request *GetFiscalAuthDataRequest) (*GetFisca
 
 // GetTicket 获取sdk临时票据
 // https://developers.weixin.qq.com/doc/service/api/invoice/FiscalReceipt/api_getticket.html
-func (c *Client) GetTicket() (*GetTicketResult, error) {
-	result := &GetTicketResult{}
-	query := url.Values{"type": {"wx_card"}}
-	err := c.Https.Get(c.ctx, "/cgi-bin/ticket/getticket", query, result)
+func (c *Client) GetTicket(ctx context.Context) (*GetTicketResult, error) {
+	token, err := c.AccessTokenE(ctx)
 	if err != nil {
+		return nil, err
+	}
+	result := &GetTicketResult{}
+	query := url.Values{
+		"access_token": {token},
+		"type":         {"wx_card"},
+	}
+	if err := c.Https.Get(ctx, "/cgi-bin/ticket/getticket", query, result); err != nil {
 		return nil, err
 	}
 	return result, nil
@@ -29,10 +41,14 @@ func (c *Client) GetTicket() (*GetTicketResult, error) {
 
 // RejectInsertFiscal 拒绝开票
 // https://developers.weixin.qq.com/doc/service/api/invoice/FiscalReceipt/api_invoicebizrejectinsert.html
-func (c *Client) RejectInsertFiscal(request *RejectInsertFiscalRequest) (*Resp, error) {
-	result := &Resp{}
-	err := c.Https.Post(c.ctx, "/card/invoice/rejectinsert", request, result)
+func (c *Client) RejectInsertFiscal(ctx context.Context, request *RejectInsertFiscalRequest) (*Resp, error) {
+	token, err := c.AccessTokenE(ctx)
 	if err != nil {
+		return nil, err
+	}
+	result := &Resp{}
+	path := fmt.Sprintf("/card/invoice/rejectinsert?access_token=%s", token)
+	if err := c.Https.Post(ctx, path, request, result); err != nil {
 		return nil, err
 	}
 	return result, nil
@@ -40,11 +56,15 @@ func (c *Client) RejectInsertFiscal(request *RejectInsertFiscalRequest) (*Resp, 
 
 // SetFiscalInvoiceUrl 设置财政电子票据授权页链接
 // https://developers.weixin.qq.com/doc/service/api/invoice/FiscalReceipt/api_setinvoiceurl.html
-func (c *Client) SetFiscalInvoiceUrl() (*SetInvoiceUrlResult, error) {
+func (c *Client) SetFiscalInvoiceUrl(ctx context.Context) (*SetInvoiceUrlResult, error) {
+	token, err := c.AccessTokenE(ctx)
+	if err != nil {
+		return nil, err
+	}
 	result := &SetInvoiceUrlResult{}
 	body := map[string]string{}
-	err := c.Https.Post(c.ctx, "/card/invoice/seturl", body, result)
-	if err != nil {
+	path := fmt.Sprintf("/card/invoice/seturl?access_token=%s", token)
+	if err := c.Https.Post(ctx, path, body, result); err != nil {
 		return nil, err
 	}
 	return result, nil
@@ -52,10 +72,14 @@ func (c *Client) SetFiscalInvoiceUrl() (*SetInvoiceUrlResult, error) {
 
 // GetPlatformPdf 查询已上传的PDF文件
 // https://developers.weixin.qq.com/doc/service/api/invoice/FiscalReceipt/api_invoiceplatformgetpdf.html
-func (c *Client) GetPlatformPdf(request *GetPlatformPdfRequest) (*GetPlatformPdfResult, error) {
-	result := &GetPlatformPdfResult{}
-	err := c.Https.Post(c.ctx, "/card/invoice/platform/getpdf", request, result)
+func (c *Client) GetPlatformPdf(ctx context.Context, request *GetPlatformPdfRequest) (*GetPlatformPdfResult, error) {
+	token, err := c.AccessTokenE(ctx)
 	if err != nil {
+		return nil, err
+	}
+	result := &GetPlatformPdfResult{}
+	path := fmt.Sprintf("/card/invoice/platform/getpdf?access_token=%s", token)
+	if err := c.Https.Post(ctx, path, request, result); err != nil {
 		return nil, err
 	}
 	return result, nil
@@ -63,10 +87,14 @@ func (c *Client) GetPlatformPdf(request *GetPlatformPdfRequest) (*GetPlatformPdf
 
 // UpdateInvoicePlatformStatus 更新发票状态
 // https://developers.weixin.qq.com/doc/service/api/invoice/FiscalReceipt/api_invoicekpupdatainvoicestatus.html
-func (c *Client) UpdateInvoicePlatformStatus(request *UpdateInvoicePlatformStatusRequest) (*Resp, error) {
-	result := &Resp{}
-	err := c.Https.Post(c.ctx, "/card/invoice/platform/updatestatus", request, result)
+func (c *Client) UpdateInvoicePlatformStatus(ctx context.Context, request *UpdateInvoicePlatformStatusRequest) (*Resp, error) {
+	token, err := c.AccessTokenE(ctx)
 	if err != nil {
+		return nil, err
+	}
+	result := &Resp{}
+	path := fmt.Sprintf("/card/invoice/platform/updatestatus?access_token=%s", token)
+	if err := c.Https.Post(ctx, path, request, result); err != nil {
 		return nil, err
 	}
 	return result, nil
@@ -74,10 +102,14 @@ func (c *Client) UpdateInvoicePlatformStatus(request *UpdateInvoicePlatformStatu
 
 // GetFiscalAuthUrl 获取授权页链接
 // https://developers.weixin.qq.com/doc/service/api/invoice/FiscalReceipt/api_notaxinvoicegetauthurl.html
-func (c *Client) GetFiscalAuthUrl(request *GetFiscalAuthUrlRequest) (*GetFiscalAuthUrlResult, error) {
-	result := &GetFiscalAuthUrlResult{}
-	err := c.Https.Post(c.ctx, "/nontax/getbillauthurl", request, result)
+func (c *Client) GetFiscalAuthUrl(ctx context.Context, request *GetFiscalAuthUrlRequest) (*GetFiscalAuthUrlResult, error) {
+	token, err := c.AccessTokenE(ctx)
 	if err != nil {
+		return nil, err
+	}
+	result := &GetFiscalAuthUrlResult{}
+	path := fmt.Sprintf("/nontax/getbillauthurl?access_token=%s", token)
+	if err := c.Https.Post(ctx, path, request, result); err != nil {
 		return nil, err
 	}
 	return result, nil
@@ -85,10 +117,14 @@ func (c *Client) GetFiscalAuthUrl(request *GetFiscalAuthUrlRequest) (*GetFiscalA
 
 // CreateFiscalCard 创建财政电子票据模板
 // https://developers.weixin.qq.com/doc/service/api/invoice/FiscalReceipt/api_notaxinvoicecreatecard.html
-func (c *Client) CreateFiscalCard(request *CreateFiscalCardRequest) (*CreateFiscalCardResult, error) {
-	result := &CreateFiscalCardResult{}
-	err := c.Https.Post(c.ctx, "/nontax/createbillcard", request, result)
+func (c *Client) CreateFiscalCard(ctx context.Context, request *CreateFiscalCardRequest) (*CreateFiscalCardResult, error) {
+	token, err := c.AccessTokenE(ctx)
 	if err != nil {
+		return nil, err
+	}
+	result := &CreateFiscalCardResult{}
+	path := fmt.Sprintf("/nontax/createbillcard?access_token=%s", token)
+	if err := c.Https.Post(ctx, path, request, result); err != nil {
 		return nil, err
 	}
 	return result, nil
@@ -96,10 +132,14 @@ func (c *Client) CreateFiscalCard(request *CreateFiscalCardRequest) (*CreateFisc
 
 // InsertFiscalInvoice 票据插入用户卡包
 // https://developers.weixin.qq.com/doc/service/api/invoice/FiscalReceipt/api_notaxinvoiceinsert.html
-func (c *Client) InsertFiscalInvoice(request *InsertFiscalInvoiceRequest) (*InsertInvoiceResult, error) {
-	result := &InsertInvoiceResult{}
-	err := c.Https.Post(c.ctx, "/nontax/insertbill", request, result)
+func (c *Client) InsertFiscalInvoice(ctx context.Context, request *InsertFiscalInvoiceRequest) (*InsertInvoiceResult, error) {
+	token, err := c.AccessTokenE(ctx)
 	if err != nil {
+		return nil, err
+	}
+	result := &InsertInvoiceResult{}
+	path := fmt.Sprintf("/nontax/insertbill?access_token=%s", token)
+	if err := c.Https.Post(ctx, path, request, result); err != nil {
 		return nil, err
 	}
 	return result, nil
@@ -107,9 +147,14 @@ func (c *Client) InsertFiscalInvoice(request *InsertFiscalInvoiceRequest) (*Inse
 
 // SetPlatformPdf 上传发票PDF
 // https://developers.weixin.qq.com/doc/service/api/invoice/FiscalReceipt/api_invoiceplatformsetpdf.html
-func (c *Client) SetPlatformPdf(pdfFilePath string) (*SetPdfResult, error) {
+func (c *Client) SetPlatformPdf(ctx context.Context, pdfFilePath string) (*SetPdfResult, error) {
+	token, err := c.AccessTokenE(ctx)
+	if err != nil {
+		return nil, err
+	}
 	result := &SetPdfResult{}
+	path := fmt.Sprintf("/card/invoice/platform/setpdf?access_token=%s", token)
 	// 使用通用的Post方法发送文件
-	err := c.Https.Post(c.ctx, "/card/invoice/platform/setpdf", pdfFilePath, result)
+	err = c.Https.Post(ctx, path, pdfFilePath, result)
 	return result, err
 }
