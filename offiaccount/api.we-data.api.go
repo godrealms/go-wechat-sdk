85acf219976f3b7cdabe46e493c94505ac4038b5
wujie <wujie812@qq.com>
2026-04-14 11:02:11 +0800

    fix(offiaccount): wire AccessTokenE in we-data interface analytics

--- offiaccount/api.we-data.api.go
+++ offiaccount/api.we-data.api.go
@@ -1,11 +1,20 @@
 package offiaccount
 
+import (
+	"context"
+	"fmt"
+)
+
 // GetInterfaceSummary 获取接口分析数据
 // beginDate: 起始日期(YYYY-MM-DD格式)
 // endDate: 结束日期(最大时间跨度30天)
-func (c *Client) GetInterfaceSummary(beginDate, endDate string) (*GetInterfaceSummaryResult, error) {
+func (c *Client) GetInterfaceSummary(ctx context.Context, beginDate, endDate string) (*GetInterfaceSummaryResult, error) {
+	token, err := c.AccessTokenE(ctx)
+	if err != nil {
+		return nil, err
+	}
 	// 构造请求URL
-	path := "/datacube/getinterfacesummary"
+	path := fmt.Sprintf("/datacube/getinterfacesummary?access_token=%s", token)
 
 	// 构造请求体
 	req := &GetDataRequest{
@@ -15,8 +24,7 @@ func (c *Client) GetInterfaceSummary(beginDate, endDate string) (*GetInterfaceSu
 
 	// 发送请求
 	var result GetInterfaceSummaryResult
-	err := c.Https.Post(c.ctx, path, req, &result)
-	if err != nil {
+	if err := c.Https.Post(ctx, path, req, &result); err != nil {
 		return nil, err
 	}
 
@@ -26,9 +34,13 @@ func (c *Client) GetInterfaceSummary(beginDate, endDate string) (*GetInterfaceSu
 // GetInterfaceSummaryHour 获取接口分析分时数据
 // beginDate: 起始日期(YYYY-MM-DD格式)
 // endDate: 结束日期(最大时间跨度1天)
-func (c *Client) GetInterfaceSummaryHour(beginDate, endDate string) (*GetInterfaceSummaryHourResult, error) {
+func (c *Client) GetInterfaceSummaryHour(ctx context.Context, beginDate, endDate string) (*GetInterfaceSummaryHourResult, error) {
+	token, err := c.AccessTokenE(ctx)
+	if err != nil {
+		return nil, err
+	}
 	// 构造请求URL
-	path := "/datacube/getinterfacesummaryhour"
+	path := fmt.Sprintf("/datacube/getinterfacesummaryhour?access_token=%s", token)
 
 	// 构造请求体
 	req := &GetDataRequest{
@@ -38,8 +50,7 @@ func (c *Client) GetInterfaceSummaryHour(beginDate, endDate string) (*GetInterfa
 
 	// 发送请求
 	var result GetInterfaceSummaryHourResult
-	err := c.Https.Post(c.ctx, path, req, &result)
-	if err != nil {
+	if err := c.Https.Post(ctx, path, req, &result); err != nil {
 		return nil, err
 	}
 
